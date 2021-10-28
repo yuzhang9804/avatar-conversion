651e3406c77b9376b99fe2a711f0b78ce9b4061d
张宇 <yuzhang9804@gmail.com>
2021-10-28 21:46:16 +0800

    ci: github page

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import WindiCSS from 'vite-plugin-windicss'
-
-export default defineConfig({
-  plugins: [react(), WindiCSS()],
-})
