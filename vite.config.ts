277089f959e3f088878346d6d804b337a0e2203d
张宇 <yuzhang9804@gmail.com>
2021-10-28 22:22:40 +0800

    docs: update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
+  base: '/avatar-conversion/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react(), WindiCSS()],
 })
